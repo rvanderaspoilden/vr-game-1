3de2032d8dcb10a3a475250cad8dd106b4ba202a
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2021-01-06 21:40:31 +0100

    :sparkles: add extinguisher

---
+++ Assets/Scripts/Extinguisher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Extinguisher : MonoBehaviour {
+    [SerializeField]
+    private ParticleSystem particles;
+
+    private void Awake() {
+        this.particles = GetComponentInChildren<ParticleSystem>();
+        this.particles.Stop();
+    }
+
+    public void Use() {
+        this.particles.Play();
+    }
+
+    public void StopUsing() {
+        this.particles.Stop();
+    }
+}
